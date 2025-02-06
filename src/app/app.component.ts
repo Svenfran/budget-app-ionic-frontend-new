3b7ef1bcc3531eaf7dc0950c11aa5f721dd6ffa0
SHI <sven.haider@qualityminds.de>
2025-02-06 11:30:43 +0100

    kleine websocket optimierung für reconnect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth/auth.service';
 import { Router } from '@angular/router';
 import * as SockJS from 'sockjs-client';
 import { environment } from 'src/environments/environment';
-import { Stomp } from '@stomp/stompjs';
+import { Client, Stomp } from '@stomp/stompjs';
 import { ShoppinglistService } from './domains/shoppinglist/shoppinglist.service';
 import { ShoppinglistDto } from './domains/shoppinglist/shoppinglist-dto';
 import { User } from './auth/user';
@@ -25,9 +25,6 @@ export class AppComponent {
 
   socketClient: any = null;
   private subscriptions: any[] = [];
-  private reconnectInterval = 5000;
-  private reconnectAttempts = 0;
-  private maxReconnectAttempts = 100;
   
   constructor(
     private authService: AuthService,
@@ -51,147 +48,153 @@ export class AppComponent {
   }
 
   connectWebSocket(userId: number, token: any) {
-    let ws = new SockJS(`${environment.apiBaseUrl}/ws`);
-    this.socketClient = Stomp.over(ws);
+    this.socketClient = new Client({
+      webSocketFactory: () => new SockJS(`${environment.apiBaseUrl}/ws`),
+      connectHeaders: { Authorization: token },
+      debug: (str) => console.log(str),
+      reconnectDelay: 5000, // Automatisches Reconnect-Intervall (5 Sekunden)
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: (frame) => {
+        console.log('Connected to WebSocket:', frame);
+        this.subscribeToTopics(userId);
+      },
+      onStompError: (frame) => {
+        console.error('STOMP-Error:', frame);
+      },
+      onWebSocketClose: () => {
+        console.warn('WebSocket geschlossen. Versuche, die Verbindung erneut herzustellen...');
+      },
+    });
+  
+    this.socketClient.activate(); // WebSocket-Verbindung starten
 
-    this.socketClient.connect({"Authorization:": token}, () => {
-      console.log("Connecting to WS...");
-      this.reconnectAttempts = 0;
-
-      this.unsubscribeAll();
-
-      this.subscriptions.push(
-        this.socketClient.subscribe(
-          `/user/${userId}/notification/add-list`,
-          (message: any) => {
-            const parsedData = JSON.parse(message.body);
-            const shoppinglistDto: ShoppinglistDto = {
-              id: parsedData.id,
-              name: parsedData.name,
-              shoppingItems: []
-            } 
-            console.log(parsedData);
-            this.shoppingLists().push(shoppinglistDto);
-          }
-        )
-      );
-
-      this.subscriptions.push(
-        this.socketClient.subscribe(
-          `/user/${userId}/notification/update-list`,
-          (message: any) => {
-            const parsedData = JSON.parse(message.body);
-            console.log(parsedData);
-            const updatedList = this.shoppingLists().map(item => 
-              item.id === parsedData.id ? {...item, name: parsedData.name} : item
-            );
-            this.shoppingLists.set(updatedList);
-          }
-        )
-      );
-
-      this.subscriptions.push(
-        this.socketClient.subscribe(
-          `/user/${userId}/notification/delete-list`,
-          (message: any) => {
-            const parsedData = JSON.parse(message.body);
-            console.log(parsedData);
-            const updatedList = this.shoppingLists().filter(item => item.id !== parsedData.id);
-            this.shoppingLists.set(updatedList);
-          }
-        )
-      );
-
-      this.subscriptions.push(
-        this.socketClient.subscribe(
-          `/user/${userId}/notification/add-item`,
-          (message: any) => {
-            const parsedData = JSON.parse(message.body);
-            console.log(parsedData);
-            const newItem: ShoppingitemDto = {
-              id: parsedData.id!,
-              name: parsedData.name,
-              completed: parsedData.completed
-            }
-
-            const updatedList = this.shoppingLists().map(list => {
-              if (list.id === parsedData.shoppingListId) {
-                return {
-                  ...list,  // Erstellt eine neue Kopie der Shoppingliste
-                  shoppingItems: [...list.shoppingItems, newItem] // Fügt das neue Item hinzu
-                };
-              }
-              return list; // Unveränderte Listen zurückgeben
-            });
-
-            this.shoppingLists.set(updatedList);
-          }
-        )
-      );
-      
-      this.subscriptions.push(
-        this.socketClient.subscribe(
-          `/user/${userId}/notification/update-item`,
-          (message: any) => {
-            const parsedData = JSON.parse(message.body);
-            console.log(parsedData);
-            const updatedItem: ShoppingitemDto = {
-              id: parsedData.id!,
-              name: parsedData.name,
-              completed: parsedData.completed
-            }
-            const updatedList = this.shoppingLists().map(list => {
-              if (list.shoppingItems.some(item => item.id === updatedItem.id)) {
-                return {
-                  ...list,
-                  shoppingItems: list.shoppingItems.map(item =>
-                    item.id === updatedItem.id ? updatedItem : item
-                  )
-                };
-              }
-              return list;
-            });
-            this.shoppingLists.set(updatedList);
-          }
-        )
-      );
-      
-      this.subscriptions.push(
-        this.socketClient.subscribe(
-          `/user/${userId}/notification/delete-item`,
-          (message: any) => {
-            const parsedData = JSON.parse(message.body);
-            console.log(parsedData);
-            const updatedList = this.shoppingLists().map(list => {
-              if (list.id === parsedData.shoppingListId) {
-                return {
-                  ...list,
-                  shoppingItems: list.shoppingItems.filter(item => item.id !== parsedData.id)
-                };
-              }
-              return list;
-            });
-            this.shoppingLists.set(updatedList);
-          }
-        )
-      );
-      
-    }, (error: any) => {
-      console.error('Websocket disconnected: ', error);
-      this.handleReconnect(userId, token);
-    })
   }
 
-  handleReconnect(userId: number, token: any) {
-    if (this.reconnectAttempts < this.maxReconnectAttempts) {
-      this.reconnectAttempts++;
-      console.log(`Reconnect-Versuch #${this.reconnectAttempts} in ${this.reconnectInterval / 1000} Sekunden...`);
-      setTimeout(() => {
-        this.connectWebSocket(userId, token);
-      }, this.reconnectInterval);
-    } else {
-      console.warn("Maximale Anzahl an Reconnect-Versuchen erreicht. Kein erneuter Verbindungsaufbau.");
-    }
+  subscribeToTopics(userId: number) {
+    this.unsubscribeAll();
+
+    this.subscriptions.push(
+      this.socketClient.subscribe(
+        `/user/${userId}/notification/add-list`,
+        (message: any) => {
+          const parsedData = JSON.parse(message.body);
+          const shoppinglistDto: ShoppinglistDto = {
+            id: parsedData.id,
+            name: parsedData.name,
+            shoppingItems: []
+          } 
+          console.log(parsedData);
+          this.shoppingLists().push(shoppinglistDto);
+        }
+      )
+    );
+
+    this.subscriptions.push(
+      this.socketClient.subscribe(
+        `/user/${userId}/notification/update-list`,
+        (message: any) => {
+          const parsedData = JSON.parse(message.body);
+          console.log(parsedData);
+          const updatedList = this.shoppingLists().map(item => 
+            item.id === parsedData.id ? {...item, name: parsedData.name} : item
+          );
+          this.shoppingLists.set(updatedList);
+        }
+      )
+    );
+
+    this.subscriptions.push(
+      this.socketClient.subscribe(
+        `/user/${userId}/notification/delete-list`,
+        (message: any) => {
+          const parsedData = JSON.parse(message.body);
+          console.log(parsedData);
+          const updatedList = this.shoppingLists().filter(item => item.id !== parsedData.id);
+          this.shoppingLists.set(updatedList);
+        }
+      )
+    );
+
+    this.subscriptions.push(
+      this.socketClient.subscribe(
+        `/user/${userId}/notification/add-item`,
+        (message: any) => {
+          const parsedData = JSON.parse(message.body);
+          console.log(parsedData);
+          const newItem: ShoppingitemDto = {
+            id: parsedData.id!,
+            name: parsedData.name,
+            completed: parsedData.completed
+          }
+
+          const updatedList = this.shoppingLists().map(list => {
+            if (list.id === parsedData.shoppingListId) {
+              return {
+                ...list,  // Erstellt eine neue Kopie der Shoppingliste
+                shoppingItems: [...list.shoppingItems, newItem] // Fügt das neue Item hinzu
+              };
+            }
+            return list; // Unveränderte Listen zurückgeben
+          });
+
+          this.shoppingLists.set(updatedList);
+        }
+      )
+    );
+    
+    this.subscriptions.push(
+      this.socketClient.subscribe(
+        `/user/${userId}/notification/update-item`,
+        (message: any) => {
+          const parsedData = JSON.parse(message.body);
+          console.log(parsedData);
+          const updatedItem: ShoppingitemDto = {
+            id: parsedData.id!,
+            name: parsedData.name,
+            completed: parsedData.completed
+          }
+          const updatedList = this.shoppingLists().map(list => {
+            if (list.shoppingItems.some(item => item.id === updatedItem.id)) {
+              return {
+                ...list,
+                shoppingItems: list.shoppingItems.map(item =>
+                  item.id === updatedItem.id ? updatedItem : item
+                )
+              };
+            }
+            return list;
+          });
+          this.shoppingLists.set(updatedList);
+        }
+      )
+    );
+    
+    this.subscriptions.push(
+      this.socketClient.subscribe(
+        `/user/${userId}/notification/delete-item`,
+        (message: any) => {
+          const parsedData = JSON.parse(message.body);
+          console.log(parsedData);
+          const updatedList = this.shoppingLists().map(list => {
+            if (list.id === parsedData.shoppingListId) {
+              return {
+                ...list,
+                shoppingItems: list.shoppingItems.filter(item => item.id !== parsedData.id)
+              };
+            }
+            return list;
+          });
+          this.shoppingLists.set(updatedList);
+        }
+      )
+    );
+    
+  }
+
+  disconnectWebSocket() {
+    this.unsubscribeAll();
+    this.socketClient.deactivate();
   }
   
   private unsubscribeAll() {
@@ -201,6 +204,11 @@ export class AppComponent {
 
   onLogout() {
     this.authService.logout();
+    this.disconnectWebSocket();
     this.router.navigateByUrl("/auth", { replaceUrl: true });
+  }
+
+  ngOnDestroy() {
+    this.disconnectWebSocket();
   }
 }
